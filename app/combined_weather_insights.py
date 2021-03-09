7ae8dafaa5ce0f0e38e00f387e9e3589b50b6e26
Justin Norton <justinnorton@Justins-Mac-mini.local>
2021-03-09 09:33:13 -0500

    worker capacity

---
+++ app/combined_weather_insights.py
@@ -0,0 +1,139 @@
+import numpy as np 
+import pandas as pd
+from pylab import mpl, plt
+import datetime as dt
+import plotly.graph_objects as go
+
+
+
+
+class OttawaCombinedProductivity(): 
+    def __init__(self, file ): 
+        self.data = pd.read_csv('daily_combined_data.csv', index_col = 0)
+        self.data = self.data.set_index(pd.DatetimeIndex(self.data.index))
+        self.month_list = ['january', 'february', 'march', 'april', 'may', 
+                           'june', 'july', 'august', 'september', 'october', 
+                           'november', 'december'] 
+        
+        self.month_dict = {1: 'january',2: 'february',3: 'march',4: 'april',5: 'may',
+                           6: 'june', 7: 'july', 8: 'august', 9: 'september',
+                           10: 'october', 11: 'november', 12: 'december'}
+        self.swap_dict = {'january':1,'february':2,'march':3, 'april': 4,'may': 5,
+                           'june': 6,'july': 7,'august': 8,'september' : 9,
+                            'october': 9, 'november': 11, 'december': 12}
+
+        self.productivity_curve()
+        self.make_monthly_data()
+        self.make_productivity_curve()
+        self.plot_productivity_by_month()
+        
+        
+    def productivity_curve(self): 
+        self.x = np.linspace(-30, 30, 10000)
+        self.y = (- .001 *self.x ** 2 + .001 * self.x + 1.) 
+        self.polyfit = np.polyfit(self.x, self.y,deg =5)
+        self.polyval = np.polyval(self.polyfit, self.x)
+        
+        return self.polyfit
+    
+    
+    def make_monthly_data(self): 
+        self.monthly_data = self.data
+        self.monthly_data['productivity'] = self.data['Mean Temp (°C)'].apply(lambda x: np.polyval(self.polyfit, x))
+        self.monthly_data = self.data.groupby([self.data.index.month, self.data.index.day]).agg('mean')
+        
+        return self.monthly_data
+    
+    def generate_work_capacity_insights(self, i =1): 
+        if i in [11, 12, 1, 2, 3, 4]: 
+            wind_chill_rules = {
+            # wind chill temperatures
+            #'normal break required': (monthly_data['extremes'].between(-26, -20)  & monthly_data['Wind Spd (km/h)'].between(0, 8)), 
+            '75m work period with 2 breaks / 4hr work block' : (self.monthly_data['extremes'].between(-26, -20) & self.monthly_data['Wind Spd (km/h)'].between(16,24)),
+            '55m work period with 3 breaks / 4hr work block' : (self.monthly_data['extremes'].between(-26, -20) & self.monthly_data['Wind Spd (km/h)'].between(24,32)),
+            '40m work period with 4 breaks / 4hr work block' : (self.monthly_data['extremes'].between(-26, -20) & self.monthly_data['Wind Spd (km/h)'].round() > 32), 
+            }
+            self.monthly_data['cool_conditions'] = np.select(wind_chill_rules.values(), wind_chill_rules.keys())
+            self.work_capacity_dict = self.monthly_data['cool_conditions'].loc[i,:].value_counts().to_dict()
+            return self.work_capacity_dict
+            
+        else: 
+            humidity_rules = {
+            '75m work period with 2 breaks / 4hr work block' : (self.monthly_data['extremes'].between(25, 30)) ,
+            '55m work period with 3 breaks / 4hr work block' : (self.monthly_data['extremes'].between(30, 35)) ,
+            '40m work period with 4 breaks / 4hr work block' : (self.monthly_data['extremes'] > 35)
+        }
+
+            self.monthly_data['humid_conditions'] = np.select(humidity_rules.values(), humidity_rules.keys())
+            self.work_capacity_dict = self.monthly_data['humid_conditions'].loc[i,:].value_counts().to_dict()
+            return self.work_capacity_dict 
+    
+   
+    
+    
+    
+    def make_productivity_curve(self): 
+        fig_p = go.Figure()
+        fig_p.add_trace(go.Scatter(x=self.x, y=self.polyval, fill='tozeroy')) 
+        fig_p.update_layout(
+            title={
+                'text': "Worker productivity vs Temperature",
+                'y':0.9,
+                'x':0.5,
+                'xanchor': 'center',
+                'yanchor': 'top'},
+            xaxis_title="Worker Productivity",
+            yaxis_title="Ambient Temperature",
+            legend_title="Legend Title",
+            font=dict(
+                family="Franklin Gothic",
+                size=12,
+                color="Black"), 
+            height = 350, 
+            width = 350)
+        
+        self.prductivity_figure = fig_p
+    
+    def plot_productivity_by_month(self, i = 1):
+        '''
+        Function that generates monthly productivity vs temperature
+        '''
+        self.tdi = self.month_dict
+        self.tdi[i] = self.tdi[i]
+        fit_x = self.monthly_data.loc[i, :].index
+        fit_y = self.monthly_data['productivity'].loc[i, :]
+        y_poly_fit = np.polyfit(fit_x, fit_y, deg = 10)
+        x_poly_val = np.polyval(y_poly_fit, fit_x)
+         
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=self.monthly_data.loc[i, :].index,
+                                 y = x_poly_val,
+                                 #y=self.monthly_data['productivity'].loc[i,:], #.loc[i,:].values, 
+                                 fill='tonexty', line_color = 'hotpink',))
+        fig.update_layout(
+            title={
+                'text': f"{self.tdi[i]} productivity",
+                'y':0.9,
+                'x':0.5,
+                'xanchor': 'center',
+                'yanchor': 'top'},
+            yaxis_range=[0,1],
+            xaxis_title="Days Of the Month",
+            yaxis_title="Productivity",
+            legend_title="Legend Title",
+            font=dict(
+                family="Franklin Gothic",
+                size=18,
+                color="Black"
+            ), 
+            height = 700, 
+            width = 1200,
+        )
+        
+        
+        self.plot_productivity_plot = fig
+        return self.plot_productivity_plot
+    
+    
+        
+    
--- app/main.py
+++ app/main.py
@@ -1,55 +1,69 @@
-import streamlit as st
+import streamlit as st 
+import numpy as np 
 import pandas as pd 
-import numpy as np
-import webbrowser
-from weather_data_processor import CSVReader
-from weather_data_analyzer import OttawaWeatherAnalytics 
+import sys 
+from pathlib import Path 
+from  combined_weather_insights import OttawaCombinedProductivity
 import os
 
 
+
+dir = Path('/usr/src/app/')
+file = 'daily_combined_data.csv'
+#st.text(file)
+
+cwi = OttawaCombinedProductivity(file = file)
 
 class ConstructionProductivityVisualizer(): 
 	'''
 	A class that builds the visualization dashboard for construvtion worker productivity vs temperature
 	'''
 	def __init__(self): 
-		self.df = CSVReader().df
-		self.ana = OttawaWeatherAnalytics(self.df)
+		self.cwi = cwi
 		st.set_page_config(layout="wide") 
 		st.image('app/fuller_logo.jpg', width = 200)
-		st.title('Construction Worker Productivity and Temperature')
-
+		st.title('Climate Factors on Construction Worker Productivity')
+		
 		st.subheader('Thomas Fuller Construction')
-		self.productivity_list = ['productive', 'less productive']
-		self.nrc_paper_url = 'https://publications-cnrc.canada.ca/eng/view/ft/?id=52dc96d5-4ba0-40e6-98d2-8d388cba30cd'
+		#self.productivity_list = ['productive', 'less productive']
+		#self.nrc_paper_url = 'https://publications-cnrc.canada.ca/eng/view/ft/?id=52dc96d5-4ba0-40e6-98d2-8d388cba30cd'
 		self.month_selector()
 		self.side_bar_information()
+		self.derive_monthly_work_capacity_insight()
+		
+
 	
 	def month_selector(self): 
 		self.month = st.select_slider('Select the month for productivity you wish to analyze',
-										options = list(self.ana.month_dict.values()))
-		for self.number_list, self.month_list in self.ana.month_dict.items():
-			if self.month_list == self.month: 
-				
-				st.write(f'For the month of {self.month} we see an average temperature productivity of \
-				{self.ana.temp_productivity_frames[self.number_list].iloc[:,1].mean().round(3) *100}%. \
-				With {np.sum(self.ana.temp_productivity_frames[self.number_list].iloc[:,1] < .5)} days below 50% productivity.  \
-				It stands that {self.month} is a \
-				{np.where(self.ana.temp_productivity_frames[self.number_list].iloc[:,1].mean() > .5 ,self.productivity_list[0], self.productivity_list[1])} \
-				month when it comes to outdoor construction.')
-				self.ana.plot_productivity_by_month(i = self.number_list)
-				st.plotly_chart(self.ana.plot_productivity_plot)
+										options = list(self.cwi.month_list))
+		fig = self.cwi.plot_productivity_by_month(i = self.cwi.swap_dict[self.month])
+		
+		st.plotly_chart(fig)
+
+	def derive_monthly_work_capacity_insight(self): 
+		if st.button(f'Derive Worker Capacities for {self.month}'):
+			st.markdown('Based on 4 hour work period we can say with 95 confidence to expect:')
+			self.sort_dict = (self.cwi.generate_work_capacity_insights(i = self.cwi.swap_dict[self.month]))
+			del self.sort_dict['0']
+			for key, value in self.sort_dict.items():
+				st.markdown(f'* {value} days working at {key}')
+
+
+
+
+		
 
 
 	
 	def side_bar_information(self): 
+		st.sidebar.title('More About This Tool')
 		self.choice = st.sidebar.selectbox('Learn more about the methodology used for this tool', 
 			('Method', 'Research', 'Assumptions', 'Data'))
 		if self.choice == 'Method':
 			st.sidebar.markdown(f'The method we used to assemble this tool was by aggregating hourly weather data from the \
 			 Ottawa airport from the last ten years.  With this we are able to see clear seasonality and assign monthly productivity \
 			  using this function represented by this curve') 
-			st.sidebar.plotly_chart(self.ana.prductivity_figure)
+			st.sidebar.plotly_chart(self.cwi.prductivity_figure)
 		elif self.choice == 'Research': 
 			st.sidebar.markdown( f'The majority of findings used for this analysis was based of the Natioanl Research Council of Canada \
 			pulication "Productivity in Construction"  which can be found here: {self.nrc_paper_url}. \
@@ -61,40 +75,6 @@ class ConstructionProductivityVisualizer():
 		else: 
 			st.sidebar.markdown(f'The data was collected from Government of Canada data site')
 
-	
-
-
-
-
 if __name__ == "__main__":
     # execute only if run as a script
     ConstructionProductivityVisualizer()
-
-
-
-
-
-
-
-
-
-
-
-
-
-#st.title('Construction Worker Productivity and Temperature')
-#st.subheader('Thomas Fuller Construction')
-
-#select month, plot, and derive  functionality 
-#productivity_list = ['productive', 'less productive']
-#month = st.select_slider('Select the month for productivity you wish to analyze',
-#	options = list(ana.month_dict.values()))
-#for number_list, month_list in ana.month_dict.items():
-	#if# month_list == month: 
-		#st.write(f'For the month of {month} we see an average temperature productivity of {ana.temp_productivity_frames[number_list].iloc[:,1].mean().round(3) * 100}%. \
-		#	With {np.sum(ana.temp_productivity_frames[number_list].iloc[:,1] < .5)} days below 50% productivity.  \
-		#	It stands that {month} is a {np.where(ana.temp_productivity_frames[number_list].iloc[:,1].mean() > .6 ,productivity_list[0], productivity_list[1] )} \
-		#	month when it comes to outdoor construction.')
-		#ana.plot_productivity_by_month(i = number_list)
-		#st.plotly_chart(ana.plot_productivity_plot)
-		
